3eb640a0b2d2f2ff5c7632bb9668fdfc5170fa96
Timur Timaev <chemradio@gmail.com>
2026-04-21 12:57:11 +0600

    updated neopixel colors / fix

--- midi_morpher/src/visual/neopx.h
+++ midi_morpher/src/visual/neopx.h
@@ -1,37 +1,36 @@
 #pragma once
-#include <Adafruit_NeoPixel.h>
 #include "../controls/pots.h"
+#include <Adafruit_NeoPixel.h>
 
 inline Adafruit_NeoPixel pixel(NEOPIXEL_COUNT, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
-void initNeoPixel()
-{
-    pixel.begin();
-    pixel.setBrightness(1); // Adjust brightness (0-255)
+void initNeoPixel() {
+  pixel.begin();
+  pixel.setBrightness(1); // Adjust brightness (0-255)
 }
 
-void updateNeoPixel(uint8_t midiValue, AnalogPot *pots, bool enabled)
-{
-    if(!enabled) { pixel.clear(); pixel.show(); return; }
-    uint8_t target = midiValue;
-
-    for (int i = 0; i < 2; i++)
-    {
-        if (pots[i].ccDisplayDirty)
-        {
-            target = pots[i].lastMidiValue;
-            if ((millis() - pots[i].ccLastDisplayDirty) > 1000)
-            {
-                pots[i].ccDisplayDirty = false;
-            }
-            break;
-        }
-    }
-
-    uint16_t hue = map(target, 0, 127, 43690, 0);
-    // Convert HSV to RGB and set LED color
-    uint32_t color = pixel.gamma32(pixel.ColorHSV(hue));
-    pixel.setPixelColor(0, color);
-    pixel.setBrightness(map(target, 0, 127, 1, 20));
+void updateNeoPixel(uint8_t midiValue, AnalogPot *pots, bool enabled) {
+  if(!enabled) {
+    pixel.clear();
     pixel.show();
+    return;
+  }
+  uint8_t target = midiValue;
+
+  for(int i = 0; i < 2; i++) {
+    if(pots[i].ccDisplayDirty) {
+      target = pots[i].lastMidiValue;
+      if((millis() - pots[i].ccLastDisplayDirty) > 1000) {
+        pots[i].ccDisplayDirty = false;
+      }
+      break;
+    }
+  }
+
+  uint16_t hue = map(target, 0, 127, 43690, 65535);
+  // Convert HSV to RGB and set LED color
+  uint32_t color = pixel.gamma32(pixel.ColorHSV(hue));
+  pixel.setPixelColor(0, color);
+  pixel.setBrightness(map(target, 0, 127, 1, 20));
+  pixel.show();
 }
